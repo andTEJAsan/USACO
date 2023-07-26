d58ca97dca7fe60465bbc752c23a1e1f4e96ae85
tejas <tejasanand.iitdcse@gmail.com>
2023-07-26 16:18:56 +0530

    initial commit

---
+++ count_equals.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int binary_upper(vector<int>& a , int x)
+
+
+
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    vector<int> a(n);
+    vector<int> b(m);
+
+    for(int i = 0 ; i < n; i++) cin >> a[i];
+    for(int i = 0 ; i < m; i++) cin >> b[i];
+
+    
+
+
+    return 0;
+}
